d73ef0c74fafcb0e0b1c0e841a29d139d75f1427
akhilmohnn <akhilmohnn@outlook.com>
2024-09-22 14:21:24 +0530

    final js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function buttonclick(val){
     document.getElementById("screen").value+=val
 }
 
-function equalClick(){
+function equalclick(){
     var text=document.getElementById("screen").value
     var result=eval(text)      //eval is a built in function that performs actions based on the operators given
     document.getElementById("screen").value=result
